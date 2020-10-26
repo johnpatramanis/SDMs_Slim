c5097585f52aa3c6947eb0a833e1f02e1a76e287
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2020-10-26 18:47:57 +0200

    Add files via upload

--- GLOBAL_CONTROL_LATEST_VERSI/bash.sh
+++ GLOBAL_CONTROL_LATEST_VERSI/bash.sh
@@ -1,15 +1,14 @@
 
-for KAY in 1
+for KAY in 1 2 3 5 8
 do
 
 for SIGMA in 2
 do
 
-slim -d seed=1000 -d sigma=$SIGMA -d K=$KAY SDM_Version_2.slim
+slim -d seed=1000 -d sigma=$SIGMA -d K=$KAY SDM_Version_2_exper.slim
 
 done
 done
 
 python3 SDM_analysis.py
 python3 compare_matrices.py 
-##3 5
